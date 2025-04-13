48f46e8a9335d7ff98e8c87c28421b35494effae
Sakshij2810 <sakshi2020jadhav@gmail.com>
2025-04-13 22:10:00 +0530

    first commit

---
+++ server/routes/taskRoutes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const { protect } = require("../middleware/auth");
+const {
+  getTasks,
+  createTask,
+  updateTask,
+  deleteTask,
+  getSingleTask,
+} = require("../controllers/taskController");
+
+router.route("/").get(protect, getTasks).post(protect, createTask);
+router
+  .route("/:id")
+  .put(protect, updateTask)
+  .delete(protect, deleteTask)
+  .get(getSingleTask);
+
+module.exports = router;
